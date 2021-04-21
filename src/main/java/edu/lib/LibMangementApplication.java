dd25d6518ef075197a8165da65c0a510765de847
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-04-21 19:04:47 +0200

    Project structure released

---
+++ src/main/java/edu/lib/LibMangementApplication.java
@@ -0,0 +1,13 @@
+package edu.lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibMangementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibMangementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/lib/LibMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibMangementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
