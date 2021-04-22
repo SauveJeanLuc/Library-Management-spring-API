ab01bd6fa41365d21b3779bd2c6b2b570e5f7379
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-04-22 18:49:41 +0200

    Another commit to resolve errors

--- src/main/java/edu/lib/model/Reader.java
+++ src/main/java/edu/lib/model/Reader.java
@@ -1,4 +1,16 @@
 package edu.lib.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Reader {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
 }
