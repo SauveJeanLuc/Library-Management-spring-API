c5c3e755a4341197f90934dd57acb8f6609af95c
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-04-22 20:50:29 +0200

    Wrote properties for Report

--- src/main/java/edu/lib/model/Report.java
+++ src/main/java/edu/lib/model/Report.java
@@ -1,4 +1,36 @@
 package edu.lib.model;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
 public class Report {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Reg_no;
+
+    @OneToOne
+    private Reader reader;
+
+    @OneToMany
+    private List<Book> books;
+
+    @Temporal(TemporalType.DATE)
+    @CreationTimestamp
+    private Date reserveDate;
+
+    @Temporal(TemporalType.DATE)
+    @CreationTimestamp
+    private Date dueDate;
+
+    @Temporal(TemporalType.DATE)
+    @CreationTimestamp
+    private Date returnDate;
+
+
+
 }
