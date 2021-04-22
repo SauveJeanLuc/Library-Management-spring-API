5be4569b29178befc70440e91a613d30d9f0346b
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-04-22 20:22:49 +0200

    Wrote properties for Staff

--- src/main/java/edu/lib/model/Staff.java
+++ src/main/java/edu/lib/model/Staff.java
@@ -1,4 +1,20 @@
 package edu.lib.model;
 
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Staff {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
 }
