eaecd8a31001a46d731fd859582b45b673ccb275
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-04-22 18:36:01 +0200

    Wrote basic properties for Book model

--- src/main/java/edu/lib/model/Book.java
+++ src/main/java/edu/lib/model/Book.java
@@ -1,4 +1,35 @@
 package edu.lib.model;
 
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Float price;
+
+    @NotNull
+    private String Category;
+
+    @NotNull
+    private String Edition;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String ISBN;
+
+    @NotNull
+    private String AuthNo;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Publisher publisher;
 }
