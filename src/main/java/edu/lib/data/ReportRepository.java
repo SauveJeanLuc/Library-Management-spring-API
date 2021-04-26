a5fb1190eac4166cb5f5c326aba81bad13a362be
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-04-26 15:35:01 +0200

    provided generic arguments for Reports.

--- src/main/java/edu/lib/data/ReportRepository.java
+++ src/main/java/edu/lib/data/ReportRepository.java
@@ -1,6 +1,11 @@
 package edu.lib.data;
 
+import edu.lib.model.Reader;
+import edu.lib.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReportRepository extends JpaRepository {
+import java.util.List;
+
+public interface ReportRepository extends JpaRepository<Report,Long> {
+
 }
