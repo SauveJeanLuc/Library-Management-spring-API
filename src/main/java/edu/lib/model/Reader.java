61d54266cbe06d3ce494dd6bd4336bcf06fe30e6
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-04-26 14:41:30 +0200

    Worked on Reader class Readiness

--- src/main/java/edu/lib/model/Reader.java
+++ src/main/java/edu/lib/model/Reader.java
@@ -29,4 +29,82 @@ public class Reader {
     @NotNull
     private String address;
 
+
+    //no-arg constructor
+    public Reader() {
+        super();
+    }
+    //all-arg(/id) constructor
+
+    public Reader(String email, String firstName, String lastName, String phoneNum, String address) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNum = phoneNum;
+        this.address = address;
+    }
+
+    //getters and setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    //override equals methods
+
+    @Override
+    public boolean equals(Object obj){
+        Reader otherReader = (Reader) obj;
+
+        if(!this.email.equals(otherReader.getEmail()))
+            System.out.println("Emails are not the same");
+        if(!this.firstName.equals(otherReader.getFirstName()))
+            System.out.println("FirstNames are not the same");
+        if(!this.lastName.equals(otherReader.getLastName()))
+            System.out.println("LastNames are not the same");
+        if(!this.phoneNum.equals(otherReader.getPhoneNum()))
+            System.out.println("PhoneNums are not the same");
+        if(!this.address.equals(otherReader.getAddress()))
+            System.out.println("Addresses are not the same");
+        return false;
+
+    }
+
 }
