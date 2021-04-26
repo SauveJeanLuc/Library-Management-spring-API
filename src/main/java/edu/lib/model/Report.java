1134938f2a86f30b1bfe2bc11887863bc3c1867d
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-04-26 15:01:09 +0200

    Worked on Report class Readiness

--- src/main/java/edu/lib/model/Report.java
+++ src/main/java/edu/lib/model/Report.java
@@ -32,5 +32,86 @@ public class Report {
     private Date returnDate;
 
 
+    //no-arg constructor
+
+    public Report(){
+        super();
+    }
+
+    //all-arg(/Id) constructor
+
+    public Report(Reader reader, List<Book> books, Date reserveDate, Date dueDate, Date returnDate) {
+        this.reader = reader;
+        this.books = books;
+        this.reserveDate = reserveDate;
+        this.dueDate = dueDate;
+        this.returnDate = returnDate;
+    }
+
+    //getters and setters
+
+    public Long getReg_no() {
+        return Reg_no;
+    }
+
+    public void setReg_no(Long reg_no) {
+        Reg_no = reg_no;
+    }
+
+    public Reader getReader() {
+        return reader;
+    }
+
+    public void setReader(Reader reader) {
+        this.reader = reader;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public Date getReserveDate() {
+        return reserveDate;
+    }
+
+    public void setReserveDate(Date reserveDate) {
+        this.reserveDate = reserveDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+
+    //override equals method
+
+    @Override
+    public boolean equals(Object obj){
+        if(!(obj instanceof Report))
+            return false;
+        Report otherReport = (Report) obj;
+        return this.Reg_no.equals(otherReport.getReg_no())&&
+                this.reader.equals(otherReport.getReader())&&
+                this.books.equals(otherReport.getBooks())&&
+                this.reserveDate.equals(otherReport.getReserveDate())&&
+                this.dueDate.equals(otherReport.getDueDate())&&
+                this.returnDate.equals(otherReport.getReturnDate());
+    }
 
 }
